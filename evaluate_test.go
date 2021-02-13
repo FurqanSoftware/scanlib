0d0a397bb11e92ac7d7d3f3d9cf04326edeecedc
Mahmud Ridwan <m@hjr265.me>
2021-02-13 21:21:16 +0600

    Remove unused code

--- evaluate_test.go
+++ evaluate_test.go
@@ -52,29 +52,3 @@ func TestEvaluate(t *testing.T) {
 		})
 	}
 }
-
-const sourceAdd = `var A, B int
-scan A, B
-check A >= 0, A < 10, B >= 0, B < 20
-eol
-eof
-`
-
-const sourceGrid = `var R, C int
-scan R, C
-check R >= 1, R < 25, C >= 1, C < 25
-eol
-var G [R]string
-for i 0 R
-	scan G[i]
-	check len(G[i]) == C
-	check re(G[i], "^[*.]+$")
-	eol
-end
-eof
-`
-
-const sourceN1018 = `var N int64
-scan N
-check N >= 1, N <= 1000000000000000000
-`
