9f5552c27f4bfab35ba22780e6d2d4b6da7ea059
Mahmud Ridwan <m@hjr265.me>
2023-11-07 10:47:07 +0600

    Update for recent Go

--- ast/parse.go
+++ ast/parse.go
@@ -5,29 +5,27 @@ import (
 	"github.com/alecthomas/participle/v2/lexer"
 )
 
-var parser = participle.MustBuild(&Source{},
-	participle.Lexer(lexer.MustSimple([]lexer.Rule{
-		{"comment", `#[^\n]*`, nil},
-		{"whitespace", `[ \t]+`, nil},
-		{"Float", `\d+\.\d*`, nil},
-		{"Int", `\d+`, nil},
-		{"String", `"(\\"|[^"])*"`, nil},
-		{"Keyword", `end|eof|eol|for|scanln|scan|var`, nil},
-		{"Type", `\b(bool|float32|float64|int|int64|string)\b`, nil},
-		{"Ident", `[a-zA-Z_][a-zA-Z0-9_]*`, nil},
-		{"Punct", `[-[!@#$%^&*()+_={}\|:;"'<,>.?/]|]`, nil},
-		{"EOL", `[\n\r]+`, nil},
-	})),
+var parser = participle.MustBuild[Source](participle.Lexer(lexer.MustSimple([]lexer.SimpleRule{
+	{"comment", `#[^\n]*`},
+	{"whitespace", `[ \t]+`},
+	{"Float", `\d+\.\d*`},
+	{"Int", `\d+`},
+	{"String", `"(\\"|[^"])*"`},
+	{"Keyword", `end|eof|eol|for|scanln|scan|var`},
+	{"Type", `\b(bool|float32|float64|int|int64|string)\b`},
+	{"Ident", `[a-zA-Z_][a-zA-Z0-9_]*`},
+	{"Punct", `[-[!@#$%^&*()+_={}\|:;"'<,>.?/]|]`},
+	{"EOL", `[\n\r]+`},
+})),
 	participle.Elide("comment"),
 	participle.Unquote("String"),
 	participle.UseLookahead(2),
 )
 
 func ParseString(filename string, s string) (*Source, error) {
-	n := Source{}
-	err := parser.ParseString(filename, s, &n)
+	n, err := parser.ParseString(filename, s)
 	if err != nil {
 		return nil, err
 	}
-	return &n, nil
+	return n, nil
 }
--- go.mod
+++ go.mod
@@ -1,8 +1,10 @@
 module git.furqansoftware.net/toph/scanlib
 
-go 1.15
+go 1.21.1
 
 require (
-	github.com/alecthomas/participle/v2 v2.0.0-alpha7
+	github.com/alecthomas/participle/v2 v2.1.0
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/stretchr/testify v1.4.0 // indirect
 )
